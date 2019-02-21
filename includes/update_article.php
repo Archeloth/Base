ba1050b501e514bb872e875241585238ea1ae6be
Barnabás <r.barnabas96@gmail.com>
2019-02-21 16:15:28 +0100

    valami baj van ...

--- includes/update_article.php
+++ includes/update_article.php
@@ -3,11 +3,12 @@
     {
         $new_title=$_POST['title'];
         $new_content=$_POST['content'];
+        $new_theme=$_POST['theme'];
         $id=$_POST['id'];
         //echo $new_title.$new_content;
         include_once "connection.php";
         
-            $sql="UPDATE articles SET title='$new_title',content='$new_content' WHERE article_id='$id';";
+            $sql="UPDATE articles SET theme='$new_theme',title='$new_title',content='$new_content' WHERE article_id='$id';";
             
 
             if(mysqli_query($conn,$sql))
