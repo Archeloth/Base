2dbe48df866605f1e68d5c70e7054b921ac2660a
Barnabás <r.barnabas96@gmail.com>
2019-03-09 12:37:43 +0100

    map and gym

---
+++ about_gym.php
@@ -0,0 +1,35 @@
+<?php
+    include 'includes/head.php';
+
+?>
+    <title>A tornateremről</title>
+</head>
+<body>
+    <?php include 'includes/nav.php'; ?>
+    <div class="container">
+        <div class="row py-3">
+            <div class="offset-sm-1 col-sm-5">
+                <h3>A tornateremről...</h3>
+                <img class="img-fluid" src="images/vr-gym.jpg" title="A tornaterem (illusztráció)">
+            </div> 
+            <div class="offset-sm-1 col-sm-5">
+                <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=19.036297202110294%2C47.336416097973%2C19.04525578022003%2C47.33962244592671&amp;layer=mapnik&amp;marker=47.338019296288245%2C19.04077649116516" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=47.33802&amp;mlon=19.04078#map=18/47.33802/19.04078">Nagyobb térkép</a></small>
+                    <small><a href="images/map.png">Ha a térkép nem működne</a></small>
+            </div>
+        </div>
+        
+        <div class="offset-sm-3 col-sm-6">
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis lacus vel efficitur dignissim. In sollicitudin rutrum nulla, eu suscipit felis volutpat in. Quisque velit odio, tincidunt dictum lorem ut, congue consequat massa. Suspendisse bibendum interdum nibh vitae pharetra. Nam nisi odio, fringilla at facilisis ac, egestas non arcu. Suspendisse commodo sem ac congue eleifend. Fusce sed eros ornare, efficitur ante eu, lobortis purus. Nunc gravida blandit magna ut tempor. Pellentesque at nunc cursus, tincidunt orci ac, finibus enim.</p>
+
+            <p>Fusce vehicula metus id purus tincidunt, quis lobortis mi pulvinar. Nam vehicula, libero at aliquet pulvinar, ligula erat faucibus velit, vitae vehicula quam enim ac urna. Quisque tincidunt elementum quam, sit amet faucibus nibh finibus eget. Morbi tempus rhoncus nibh, vitae malesuada magna viverra non. Sed dictum turpis tortor, at fermentum nisl dictum sollicitudin. Cras dictum pharetra mauris et hendrerit. Aenean ultricies eros a risus consequat vehicula. Fusce eleifend gravida velit eget condimentum. Aliquam sapien ipsum, porttitor bibendum semper et, venenatis id nisl. Morbi a urna sed quam congue dapibus. Vivamus faucibus tellus laoreet hendrerit maximus. Suspendisse vel vestibulum felis. Nam turpis massa, porttitor congue ipsum a, tristique vestibulum justo.</p>
+
+            <p>Sed ligula neque, posuere vitae nulla at, hendrerit auctor velit. Mauris eleifend faucibus neque, at volutpat urna sodales vulputate. Vestibulum quis turpis blandit, venenatis odio eu, ultrices justo. Donec mattis id lectus eu tempor. Etiam pellentesque massa at ultrices aliquet. Praesent in congue dolor. Etiam odio eros, sagittis sed commodo non, facilisis eu diam. Mauris lacinia arcu ut eros lobortis aliquet. Mauris dapibus eleifend sagittis. Morbi nec sem ante. Duis condimentum turpis id sodales ultricies. In scelerisque neque non dapibus tempus. Ut volutpat dictum massa, a volutpat velit ultricies vel. Curabitur eget luctus dolor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque dignissim vitae nibh et pellentesque.</p>
+
+            <p>Quisque porta venenatis molestie. In volutpat non tellus eleifend aliquet. Vestibulum quis sem ante. Sed vitae ultricies quam. Nam viverra est elit, ut sagittis mi tincidunt a. Curabitur posuere purus eget lacus porta, ac pulvinar velit mattis. Vivamus id urna et orci vulputate laoreet. Morbi sodales turpis et nulla consequat mollis. Donec molestie lorem id massa mattis, non mattis lacus bibendum. In vitae neque convallis, ultrices turpis vulputate, egestas dui.</p>
+
+            <p>Sed lobortis elit sit amet massa facilisis, non scelerisque urna fringilla. Ut dapibus tortor ut consequat maximus. Nunc consequat arcu ut feugiat lacinia. Morbi dictum dolor pulvinar elit auctor, in tincidunt magna tincidunt. Vestibulum luctus dignissim nisi, non tincidunt enim vestibulum eu. In hac habitasse platea dictumst. Donec eu elit ut urna accumsan facilisis in sed felis. Proin varius fermentum iaculis. Suspendisse fringilla, nulla sed pellentesque sollicitudin, dolor purus efficitur nulla, nec pellentesque diam risus a nibh.</p>
+        </div>
+    </div>
+    <?php include 'includes/footer.php'; ?>
+</body>
+</html>
--- includes/nav.php
+++ includes/nav.php
@@ -11,7 +11,7 @@
             <a class="nav-link" href="#">A honlapról</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Tornateremről</a>
+            <a class="nav-link" href="about_gym.php">Tornateremről</a>
           </li>
           
           <?php
